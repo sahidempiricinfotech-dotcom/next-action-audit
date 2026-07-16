3b94c9e44b190a3639fc337dbcc35003ccfa4cfe
sahidempiricinfotech-dotcom <sahid.empiricinfotech@expert.micro1.ai>
2026-07-16 10:18:28 +0530

    Create page.tsx

---
+++ app/(marketing)/cart/page.tsx
@@ -0,0 +1,10 @@
+import { CartWidget } from "@/src/components/CartWidget";
+
+export default function CartPage() {
+  return (
+    <main>
+      <h1>Your cart</h1>
+      <CartWidget />
+    </main>
+  );
+}
